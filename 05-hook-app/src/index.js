dac1b7e30ba0cd5294dcfadd833cc869f4903041
josemiranda989 <josemiranda989@gmail.com>
2022-04-04 13:16:09 -0300

    clase 112

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'; 
 // import { CountrApp } from './components/01-useState/CountrApp';
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<SimpleForm />, document.getElementById("root"));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
 
  
